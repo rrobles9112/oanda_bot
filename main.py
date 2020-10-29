cce8f0f2bfbd359f52b69a38ac7f9374efb98d6f
Richard Robles <rrobles9112@gmail.com>
2020-10-29 13:40:20 -0500

    basic run config

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ class MyBot(Bot):
             fast_ma.shift() >= slow_ma.shift()
         )
 
-MyBot(
-    account_id='<your practice account id>',
-    access_token='<your practice access token>',
-).run()
+
+if __name__ == '__main__':
+    MyBot(
+        account_id='101-011-13291497-002',
+        access_token='7c743f29b9cd0554b794f741a4c65ca7-4cace56fcf2c67ed8a5822d8eea9512b',
+    ).run()
